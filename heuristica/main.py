a283b4010f48bb73874d573449cf06643149cde0
Dayna Caroline <dayna.caroline@unesp.br>
2025-09-18 17:22:29 -0300

    adicionando inteligência grafo romênia

--- heuristica/main.py
+++ heuristica/main.py
@@ -1,15 +1,15 @@
 from greedy import greedy_best_first_search
 from a_star import a_star_search
-from graphs import graph_ag
-from heuristics import heuristic_ag
+from graphs import graph_romania
+from heuristics import heuristic_romania
 
-def run_ag():
-    start, goal = 'A', 'G'
-    print(f'=== A-G GRAPH (start: {start} -> goal: {goal}) ===')
-    g_path, g_order, g_cost = greedy_best_first_search(graph_ag, start, goal, heuristic_ag)
+def run_romania():
+    start, goal = 'Arad', 'Bucharest'
+    print(f'=== ROMANIA GRAPH (start: {start} -> goal: {goal}) ===')
+    g_path, g_order, g_cost = greedy_best_first_search(graph_romania, start, goal, heuristic_romania)
     print('[Greedy] path:', g_path, 'explored:', g_order, 'cost:', g_cost)
-    a_path, a_order, a_cost = a_star_search(graph_ag, start, goal, heuristic_ag)
+    a_path, a_order, a_cost = a_star_search(graph_romania, start, goal, heuristic_romania)
     print('[A*]     path:', a_path, 'explored:', a_order, 'cost:', a_cost)
 
 if __name__ == '__main__':
-    run_ag()
+    run_romania()
